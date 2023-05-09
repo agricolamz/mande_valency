5f1011e1b1c0d27140b78386ce697f7bd8e9c735
George Moroz <agricolamz@gmail.com>
2023-05-09 11:33:19 +0300

    update Kono etymology

--- code.R
+++
@@ -1,8 +0,0 @@
-library(tidyverse)
-df <- read_csv("data/raw_data.csv")
-
-df %>% 
-  filter(!is.na(orthography)) %>% 
-  select(id, Russian, English, orthography, transcription, prs, form2, form3, pos, `additional forms`, kk, jena200, gld110) %>% 
-  arrange(id) %>% 
-  write_csv("data/data.csv")
